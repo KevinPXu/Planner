bb71534c165b7b5bc7b7a669232d0fb442f64807
KevinPXu <kevinpxu21@gmail.com>
2022-10-05 11:44:14 -0500

    added time of day statement to java script to track when the time is

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var mHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 initLocalStorage();
 addCurrentDate();
+createTextArea();
 
 function addCurrentDate() {
   currentDayEl.text(moment().format("MMM Do YYYY"));
 }
-createTextArea();
+
 function createTextArea() {
   for (var [i, mHour] of Object.entries(mHours)) {
     var newDivEl = $("<div>");
@@ -48,7 +49,6 @@ function saveText(input, hour) {
     initLocalStorage();
     schedule = JSON.parse(localStorage.getItem("schedule"));
   }
-
   schedule[hour] = input;
   localStorage.setItem("schedule", JSON.stringify(schedule));
 }
@@ -57,5 +57,35 @@ function renderFromLocalStorage() {
   var schedule = JSON.parse(localStorage.getItem("schedule"));
   for (var hour of Object.keys(schedule)) {
     $("textarea." + hour).text(schedule[hour]);
+  }
+  timeOfDay();
+}
+
+function timeOfDay() {
+  for (var [i, mHour] of Object.entries(mHours)) {
+    if (
+      containerEl.children().children("textarea").attr("class").split(" ")[
+        mHour
+      ] < moment().hour()
+    ) {
+      containerEl
+        .children()
+        .children("textarea")
+        .attr("class", mHour + " col-xl-10 " + hours[i] + " past");
+    } else if (
+      containerEl.children().children("textarea").attr("class").split(" ")[
+        mHour
+      ] > moment().hour()
+    ) {
+      containerEl
+        .children()
+        .children("textarea")
+        .attr("class", mHour + " col-xl-10 " + hours[i] + " future");
+    } else {
+      containerEl
+        .children()
+        .children("textarea")
+        .attr("class", mHour + " col-xl-10 " + hours[i] + " future");
+    }
   }
 }
