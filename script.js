fd3955ee5cfcdcda0c0b98f08310b09164c73ec1
KevinPXu <kevinpxu21@gmail.com>
2022-10-04 21:58:56 -0500

    added dynamically created text areas with buttons that save text to local storage and renders the text back to the page

---
+++ script.js
@@ -0,0 +1,51 @@
+var currentDayEl = $("#currentDay");
+var containerEl = $(".container");
+var mHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+initLocalStorage();
+addCurrentDate();
+function addCurrentDate() {
+  currentDayEl.text(moment().format("MMM Do YYYY"));
+}
+createTextArea();
+function createTextArea() {
+  for (var [i, mHour] of Object.entries(mHours)) {
+    var newDivEl = $("<div>");
+    var textAreaEl = $("<textarea>");
+    var buttonEl = $("<button>");
+    var timeBlockEl = $("<span>");
+    newDivEl.addClass("row");
+    containerEl.append(newDivEl);
+    timeBlockEl.addClass("col-xl-1 time-block hour");
+    timeBlockEl.text(hours[i]);
+    newDivEl.append(timeBlockEl);
+    textAreaEl.addClass(mHour + " col-xl-10 " + hours[i]);
+    newDivEl.append(textAreaEl);
+    buttonEl.addClass("btn btn-primary saveBtn col-xl-1");
+    newDivEl.append(buttonEl);
+    buttonEl.on("click", function (event) {
+      var input = $(event.target).siblings().next().val();
+      var hour = $(event.target).siblings().next().attr("class").split(" ")[0];
+      saveText(input, hour);
+    });
+  }
+  renderFromLocalStorage();
+}
+
+function initLocalStorage() {
+  var schedule = {};
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+function saveText(input, hour) {
+  var schedule = JSON.parse(localStorage.getItem("schedule"));
+  schedule[hour] = input;
+  localStorage.setItem("schedule", JSON.stringify(schedule));
+}
+
+function renderFromLocalStorage() {
+  var tests = JSON.parse(localStorage.getItem("schedule"));
+  for (var test of Object.keys(tests)) {
+    console.log(test);
+  }
+}
